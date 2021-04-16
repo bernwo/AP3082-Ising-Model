d15ccc749f46c69f90c9be239bb4bc3db4b8bdf9
kwo <kahjenwo2-c@my.cityu.edu.hk>
2021-04-16 23:10:01 +0200

    Add gif-generating .py script

---
+++ generate_Ising_gif.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.constants import Boltzmann as kB
+from Metropolis_functions import*
+
+# INITIALISATION.
+N = 100000 # total number of runs in each Metropolis2D simulation
+T_factor = 0.01 # for taking fractions of the critical temperature
+J = 2 # J > 0 is a coupling constant
+Tc = 2.27 * J/kB # critical temperature for 2D square lattice Ising model
+T = T_factor * Tc
+L = 64 # number of sites along a direction
+h = 0 # external magnetic field
+spins = init_rand_lattice(L) # Begin from a random spin lattice.
+
+# Begin 1 set of Metropolis2D simulation with N runs.
+final_spins = Metropolis2D(N,spins,J,T,h,creategif=True,plot_interval=5000)
+print("Check the gif in the simulations_images folder.")
