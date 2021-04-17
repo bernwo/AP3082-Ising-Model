f4ab616b1f2eb143dcee487fe5e587a8e535c6bd
kwo <kahjenwo2-c@my.cityu.edu.hk>
2021-04-17 14:21:34 +0200

    Add docstrings

--- Metropolis_functions.py
+++ Metropolis_functions.py
@@ -7,33 +7,144 @@ import imageio # sudo pip3 install imageio
 
 # Let the total number of sites be N × N where N∈ℤ
 def init_rand_lattice(L):
-    """Generate a uniform random sample from np.array([-1,1],dtype=int) of size (L,L)"""
-    return np.random.choice(np.array([-1,1],dtype=int), (L,L))
+    """
+    Generates a lattice of size L×L with spins randomly sampled from {-1,1} with uniform distribution.
+
+    Parameters:
+    -----------
+    L: int
+        Dimension of the lattice.
+
+    Return:
+    --------
+    spins: numpy.ndarray
+        The lattice containing spins.
+    """
+    spins = np.random.choice(np.array([-1,1],dtype=int), (L,L))
+    return spins
 
 def init_pos_lattice(L):
-    """Generate a homogeneous spin lattice with spins = +1"""
-    return np.ones([L,L],dtype=int)
+    """
+    Generates a homogeneous lattice of size L×L with all spins = +1.
+
+    Parameters:
+    -----------
+    L: int
+        Dimension of the lattice.
+
+    Return:
+    --------
+    spins: numpy.ndarray
+        The lattice containing spins.
+    """
+    spins = np.ones([L,L],dtype=int)
+    return spins
 
 def init_neg_lattice(L):
-    """Generate a homogeneous spin lattice with spins = -1"""
-    return -np.ones([L,L],dtype=int)
+    """
+    Generates a homogeneous lattice of size L×L with all spins = -1.
+
+    Parameters:
+    -----------
+    L: int
+        Dimension of the lattice.
+
+    Return:
+    --------
+    spins: numpy.ndarray
+        The lattice containing spins.
+    """
+    spins = -np.ones([L,L],dtype=int)
+    return spins
 
 def get_energy_singlespin(J,h,neighbour_sums,spins):
-    return -J/2 * neighbour_sums * spins - h * spins
+    """
+    Calculates the individual spin energy contribution of every spin in the lattice.
+
+    Parameters:
+    -----------
+    J: float
+        Coupling constant. J should always be J > 0.
+    h: float
+        External magnetic field strength.
+    neighbour_sums: numpy.ndarray
+        The sum of nearest-neighbours of each spin for every spins.
+    spins: numpy.ndarray
+        The lattice containing spins.
+    
+    Return:
+    --------
+    E: numpy.ndarray
+        The individual spin energy contribution of every spin in the lattice.
+    """ 
+    E = -J/2 * neighbour_sums * spins - h * spins
+    return E
 
 def get_energy_total(E):
-    return np.sum(E)
+    """
+    Calculates the total spin energy contribution of the lattice.
+
+    Parameters:
+    -----------
+    E: numpy.ndarray
+        The individual spin energy contribution of every spin in the lattice.
+    
+    Return:
+    --------
+    E_tot: float
+        The total spin energy contribution of the lattice.
+    """ 
+    E_tot = np.sum(E)
+    return E_tot
 
 def flip_a_spin(spins):
+    """
+    Randomly flips a spin in the lattice.
+
+    Parameters:
+    -----------
+    spins: numpy.ndarray
+        The lattice containing spins.
+    
+    Return:
+    --------
+    new_spins: float
+        The lattice containing spins with a randomly flipped spin.
+    """ 
     Lx = spins.shape[0]
     Ly = spins.shape[1]
-    temp = np.copy(spins) # use np.copy() so it doesn't mutate the original numpy array.
+    new_spin = np.copy(spins) # use np.copy() so it doesn't mutate the original numpy array.
     i = np.random.randint(low=0,high=Lx)
     j = np.random.randint(low=0,high=Ly)
-    temp[i,j] = np.negative(temp[i,j])
-    return temp
+    new_spin[i,j] = np.negative(new_spin[i,j])
+    return new_spin
 
 def Metropolis2D(N,spins,J,T,h,creategif=False,plot_interval=100):
+    """
+    Executes the Metropolis algorithm for the 2D Ising model.
+
+    Parameters:
+    -----------
+    N: int
+        Total number of steps in the Metropolis algorithm to run.
+    spins: numpy.ndarray
+        The lattice containing spins.
+    J: float
+        Coupling constant. J should always be J > 0.
+    T: float
+        Temperature of the system.
+    h: float
+        External magnetic field strength.
+    creategif: bool
+        If True, the function creates an animated .gif, else it does not.
+    plot_interval: int
+        Specifies the interval between each frame in the animated .gif. Note that plot_interval should be smaller than N.
+
+    Return:
+    --------
+    final_spins: float
+        The resulting lattice after executing the Metropolis algorithm after N steps.
+    """
     # begin Metropolis algorithm
     print(f"Start Metropolis2D algorithm with {N} runs.")
     print(f"Temperature ratio, T/Tc = {np.round(T/(2.27 * J/kB),4)}")
@@ -82,4 +193,5 @@ def Metropolis2D(N,spins,J,T,h,creategif=False,plot_interval=100):
                 os.remove(filename)
             print(f"Gif created.")
 
-    return np.copy(spins)
+    final_spins = np.copy(spins)
+    return final_spins
