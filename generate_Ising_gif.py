87e3499c0be539abeee16f4b54cb45a9c439b64a
kwo <kahjenwo2-c@my.cityu.edu.hk>
2021-04-24 22:31:19 +0200

    update to use optimised Metropolis algorithm

--- generate_Ising_gif.py
+++ generate_Ising_gif.py
@@ -4,17 +4,17 @@ from scipy.constants import Boltzmann as kB
 from Metropolis_functions import*
 
 # INITIALISATION.
-N = 200000 # total number of runs in each Metropolis2D simulation
-T_factor = 1.1 # for taking fractions of the critical temperature
+N = 100000 # total number of runs in each Metropolis2D simulation
+T_factor = 0.01 # for taking fractions of the critical temperature
 J = 2 # J > 0 is a coupling constant
 Tc = 2.27 * J/kB # critical temperature for 2D square lattice Ising model
 T = T_factor * Tc
-L = 40 # number of sites along a direction
+L = 128 # number of sites along a direction
 h = 0 # external magnetic field
 spins = init_rand_lattice(L) # Begin from a uniformly random spin lattice.
 
 # Begin 1 set of Metropolis2D simulation with N runs.
-final_spins = Metropolis2D(N,spins,J,T,h,creategif=False,plot_interval=5000)
+final_spins = Metropolis2D(N,spins,J,T,h,L,creategif=True,plot_interval=5000)
 print("Check the gif in the simulations_images folder.")
 
 plt.imshow(final_spins,cmap='magma')
