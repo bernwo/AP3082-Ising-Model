08b3ad134de511488cb3990bcad0c20d36fd44b5
juandaanieel <juandadospuntostres@gmail.com>
2021-05-14 20:19:40 +0200

    autocorrelation errors

---
+++ scripts/__init__.py
@@ -0,0 +1,6 @@
+from .errors import *
+from .evolution import *
+from .wolff import *
+from .metropolis import *
+from .observables import *
+from .lattice_functions import *
--- scripts/evolution.py
+++ scripts/evolution.py
@@ -7,7 +7,7 @@ import imageio
 import os
 
 def create_gif(n_frames,relax):
-    with imageio.get_writer("simulation_images/Metropolis.gif", mode='I') as writer:
+    with imageio.get_writer("../simulation_images/Metropolis.gif", mode='I') as writer:
         print(f"Creating gif and clearing the temporary images...")
         for i in range(0,n_frames,relax):
             filename=f"simulation_images/wolff_{i}.png"
@@ -28,7 +28,7 @@ def save_frame(i,relax,spins,magnetization,T,h=0,J=1):
         plt.title(f"Magnetization.")
         plt.xlabel('T/Tc')
         plt.ylim(0,1.1)
-        plt.savefig(f"simulation_images/wolff_{i}.png", dpi=200)
+        plt.savefig(f"../simulation_images/wolff_{i}.png", dpi=200)
 
         plt.close()
 
@@ -36,9 +36,11 @@ def evolution(f,relax,L,T,dT,J=1,dJ=0):
     lat = lf.init_pos_lattice(L)
     i=0
     magnetization = [[],[],[]]
+    frames = []
 
     while T/2.27<2.5 and T>0:
         lat = f(lat,T)
+        
         if i%relax==0:
             M = get_magnetization(lat)
             magnetization[0].append(T/2.27)
@@ -46,6 +48,8 @@ def evolution(f,relax,L,T,dT,J=1,dJ=0):
             magnetization[2].append(M-get_error_mag(T))
             T += dT
             J += dJ
-            save_frame(i,relax,lat,magnetization,T)
+            frames.append(lat)
+            #save_frame(i,relax,lat,magnetization,T)
         i += 1
-    create_gif(i,relax)
+    #create_gif(i,relax)
+    return frames, magnetization
